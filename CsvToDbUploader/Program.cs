c0bd2a7daef81c81fdb00f3463f99d8a3a22d3bd
artko <artko@OMEN-RYZEN>
2022-02-06 12:35:04 +0100

    Add project files.

---
+++ CsvToDbUploader/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ReadDataFromCSVFile
+{
+    static class Program
+    {
+        static void Main()
+        {
+            string csv_file_path = @"C:\Users\artko\Desktop\ListaSzkolPolska_24.11.2021\combined-csv-files.csv";
+            DataTable csvData = GetDataTabletFromCSVFile(csv_file_path);
+            Console.WriteLine("Rows count:" + csvData.Rows.Count);
+            Console.ReadLine();
+            InsertDataIntoSQLServerUsingSQLBulkCopy(csvData);
+            Console.ReadLine();
+        }
+        private static DataTable GetDataTabletFromCSVFile(string csv_file_path)
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(csv_file_path))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = true;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        //Making empty value as null
+                        for (int i = 0; i < fieldData.Length; i++)
+                        {
+                            if (fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+                        }
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return csvData;
+        }
+
+        private static void InsertDataIntoSQLServerUsingSQLBulkCopy(DataTable csvFileData)
+        {
+            using (SqlConnection dbConnection = new SqlConnection("Server=localhost;Database=School_db;Trusted_Connection=True;"))
+            {
+                dbConnection.Open();
+                using (SqlBulkCopy s = new SqlBulkCopy(dbConnection))
+                {
+                    s.DestinationTableName = "School";
+
+                    foreach (var column in csvFileData.Columns)
+                        s.ColumnMappings.Add(column.ToString(), column.ToString());
+
+                    s.WriteToServer(csvFileData);
+                }
+            }
+        }
+    }
+}
